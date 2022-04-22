822b8a82c4a7dc2f3131a60799f13d55cadf8bc9
Wanner HernandezRiveron <84465505+Wanner-Hernandez@users.noreply.github.com>
2022-04-22 17:49:55 -0400

    Add files via upload

---
+++ CMSC430_FinalProjectimages/CMSC430Project4/listing.h
@@ -0,0 +1,17 @@
+/*
+ Compiler Theory and Design
+ CMSC 430 Week 8 Project 4
+ Wanner HernandezR
+ Professor: Dr. Duane J. Jarc
+ May 10, 2021
+ Desc: This file contains the function prototypes for the functions that produce the 
+ compilation listing
+*/
+enum ErrorCategories {LEXICAL, SYNTAX, GENERAL_SEMANTIC, DUPLICATE_IDENTIFIER,
+	UNDECLARED};
+
+void firstLine();
+void nextLine();
+int lastLine();
+void appendError(ErrorCategories errorCategory, string message);
+
---
+++ CMSC430_FinalProjectimages/CMSC430Project4/types.h
@@ -0,0 +1,19 @@
+/* 	
+ Compiler Theory and Design
+ CMSC 430 Week 8 Project 4
+ Wanner HernandezR
+ Professor: Dr. Duane J. Jarc
+ May 10, 2021
+ Desc:This file contains type definitions and the function prototypes for the type checking functions
+*/
+typedef char* CharPtr;
+
+enum Types {MISMATCH, INT_TYPE, REAL_TYPE, BOOL_TYPE, EMPTY};
+
+void checkAssignment(Types lValue, Types rValue, string message);
+Types checkArithmetic(Types left, Types right);
+Types checkLogical(Types left, Types right);
+Types checkRelational(Types left, Types right);
+Types checkInteger(Types left, Types right);
+Types checkIFThen(Types expression, Types left, Types right);
+Types checkExpressions(Types expression, Types left, Types right);
